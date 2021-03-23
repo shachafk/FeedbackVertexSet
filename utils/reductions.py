8fa09333364eab9fb9dd1e450b11d6c2a111a8e6
maiya <maiyaacovi@gmail.com>
2021-03-23 17:29:21 +0200

    added reduction 2

--- utils/reductions.py
+++ utils/reductions.py
@@ -1,10 +1,22 @@
 from typing import List
+import networkx as nx
+from utils.functions import *
+from networkx.drawing.nx_agraph import to_agraph
+import graphviz
+from networkx.drawing.nx_pydot import write_dot
 
-from networkx import MultiGraph, Graph
 
+# def reduction1(self, g: nx.MultiGraph, k: int) -> (int, List[int], bool):
+def reduction2(self, g: nx.MultiGraph, k: int):
+    changed = False
+    for u in g.nodes():
+        for v in g.neighbors(u):
+            while g.number_of_edges(u, v) > 2:
+                g.remove_edge(u, v)
+                changed = True
 
-def reduction1(self, g: MultiGraph, k: int) -> (int, List[int], bool):
-def reduction2(self, g: MultiGraph, k: int) -> (int, List[int], bool):
-def reduction3(self, g: MultiGraph, k: int) -> (int, List[int], bool):
-def reduction4(self, g: MultiGraph, k: int) -> (int, List[int], bool):
-def reduction5(self, g: MultiGraph, k: int) -> (int, List[int], bool):
+    return k, None, changed
+
+# def reduction3(self, g: nx.MultiGraph, k: int) -> (int, List[int], bool):
+# def reduction4(self, g: nx.MultiGraph, k: int) -> (int, List[int], bool):
+# def reduction5(self, g: nx.MultiGraph, k: int) -> (int, List[int], bool):
